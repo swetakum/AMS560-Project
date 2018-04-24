c225b61913fde9d834956db5fdbfcdb29a087c19
Alok Thatikunta <alok123thatikunta@gmail.com>
2018-04-23 20:52:20 -0400

    Streaming data + SQL query

--- src/main/scala/Reader.scala
+++ src/main/scala/Reader.scala
@@ -1,20 +1,55 @@
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark._
+import org.apache.spark.streaming._
+// {Seconds, StreamingContext}
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Logger, Level}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
 
 object Reader {
 	def main(args: Array[String]) {
 		Logger.getLogger("org").setLevel(Level.OFF)
 		Logger.getLogger("akka").setLevel(Level.OFF)
 
-		val sparkConf = new SparkConf().setAppName("Reader")
+		val sparkConf = new SparkConf()
+			.setAppName("Reader")
+			.set("spark.driver.allowMultipleContexts", "true")
+
 		val ssc = new StreamingContext(sparkConf, Seconds(10))
+		val sc = new SparkContext(sparkConf)
+
+		val spark = SparkSession.builder.getOrCreate()
+		import spark.implicits._
 
 		val lines = ssc.textFileStream("Data/Live/")
 
-		val words = lines.flatMap(_.split(" "))
-		val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
-		wordCounts.print()
+		val csvSchema = new StructType()
+			.add("col1", IntegerType)
+			.add("col2", IntegerType)
+			.add("col3", IntegerType)
+
+		def row(line: List[String]): Row = Row(line(0).toInt, line(1).toInt, line(2).toInt)
+
+		var state = spark.createDataFrame(sc.emptyRDD[Row], csvSchema)
+
+
+		lines.foreachRDD{ rdd =>
+			val data = rdd.map(_.split(" ").to[List]).map(row)
+			var df = spark.createDataFrame(data, csvSchema)
+			
+			state = state.union(df)
+			// state.show()
+
+			// df.createOrReplaceTempView("people")
+	    // val sqlDf = spark.sql("SELECT AVG(col2) FROM people WHERE col3 > (SELECT AVG(col3) FROM people)")
+	    // sqlDf.show()
+
+			// state.printSchema()
+
+			var ns = state
+			ns.show()
+		}
 
 		ssc.start()
 		ssc.awaitTermination()
